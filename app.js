7ff021a38737be1b3e24bc873375598465c1a9a8
Alita Kendrick <alitakendrick@Alitas-MacBook-Pro.local>
2017-03-11 10:56:23 -0600

    update

---
+++ app.js
@@ -0,0 +1,11 @@
+
+  var config = {
+    apiKey: "AIzaSyAWpzC4G8HMign764v1_CE02b-Rg4moB48",
+    authDomain: "test-test-63488.firebaseapp.com",
+    databaseURL: "https://test-test-63488.firebaseio.com",
+    storageBucket: "test-test-63488.appspot.com",
+    messagingSenderId: "936384974675"
+  };
+
+  
+  firebase.initializeApp(config);
